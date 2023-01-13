7f01784c50dd5f221bb7d56868070f76722239bf
ashwiii <achuzbro@gmail.com>
2023-01-13 09:07:35 +0530

    a23

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -8,6 +8,10 @@ import { Router } from '@angular/router';
 })
 export class DashboardComponent implements OnInit {
 
+  activeTab:string = 'Personal Details';
+  onTabClick(tab:any){
+    this.activeTab = tab;
+  }
   constructor(private route:Router) { }
 
   ngOnInit(): void {
